42393a90e776aa7c3c63220de59bad82b22f0a44
Newton Munene <newtonmunene99@gmail.com>
2020-11-12 11:47:47 +0300

    chore(package.json): Update npm dependencies

--- src/controllers/BotController.ts
+++ src/controllers/BotController.ts
@@ -16,15 +16,15 @@ export class BotController {
       .then((result: any) => {
         // We send the fulfillment text received back to our user via Twilio
         sendMessage(From, To, result.fulfillmentText)
-          .then(res => {
+          .then((res) => {
             console.log(res);
           })
-          .catch(error => {
+          .catch((error) => {
             console.error("error is ", error);
             Logger.Err(error);
           });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("error is ", error);
         Logger.Err(error);
       });
